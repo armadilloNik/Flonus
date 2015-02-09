831449e91b788338814325fe35f52dbdde4a3f16
nik <nik@tumpedOver.com>
2015-02-08 21:02:07 -0600

    meager beginnings of the unit step

---
+++ Flonus.Core/IUnitStep.cs
@@ -0,0 +1,11 @@
+﻿
+// ReSharper disable TypeParameterCanBeVariant
+
+namespace Flonus.Core
+{
+    public interface IUnitStep<TInput, TOutput>
+    {
+        void Accept(TInput input);
+        TOutput Return();
+    }
+}
---
+++ Flonus.Tests/UnitStepScratch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Flonus.Core;
+
+namespace Flonus.Tests
+{
+    public interface IMoveableFile { }
+    public interface IMovedFile { }
+
+    public class FileMover : IUnitStep<IMoveableFile, IMovedFile>
+    {
+        public void Accept(IMoveableFile input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMovedFile Return()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
